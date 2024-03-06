c2a8608b3d74470a8a6392318de25643a8776d06
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-03-05 23:10:30 -0600

    added jsx

--- views/index.jsx
+++ views/index.jsx
@@ -1,15 +1,25 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index ({ breads }) {
-   const display = breads.map((bread, i) => {
-    return <li key={i}>{bread.name}</li>
-   })
+function Index({ breads }) {
+    const display = breads.map((bread, i) => {
+        return (
+        <li key={i}>
+          <a href={`/bread/${i}`}>{bread.name}</a>
+        </li>
+        )
+    })
     return (
       <Default>
         <h2>Index Page</h2>
         <ul>
-            {display}
+            <div className="newButton">
+          <a href="/bread/new"><button>Add a new bread</button></a>
+        </div>
+        <div className="backButton">
+          <a href="/bread"><button>Go back to the index</button></a>
+          {display}
+        </div>
         </ul>
       </Default>
     )
